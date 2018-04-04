d88f7fdca075a1ec8354066a5a2fb5916caafa31
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-04-04 09:21:19 -0700

    CDP-566: Update plugins to use languages from API
    Another small issue when API is down.

--- includes/class-language-config.php
+++ includes/class-language-config.php
@@ -45,7 +45,7 @@ class Language_Helper {
       'url' => 'language'
     ];
     $data = $feeder->es_request($args);
-    if ( $data && count($data) && !$data->error ) {
+    if ( $data && count( $data ) && (!array_key_exists( 'error', $data ) || !$data[ 'error' ]) && (!is_object( $data ) || !$data->error) ) {
       $this->languages = [];
       foreach ( $data as $lang ) {
         $this->languages[$lang->locale] = $lang;
